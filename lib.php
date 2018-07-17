d22148e212a1eb38a9772c69cbac0ae18e264195
Rodrigo Mady <rodrigo.mady@gmail.com>
2018-07-16 21:28:04 -0300

    [UPDATE] #7 Simplify lib for get files

--- lib.php
+++ lib.php
@@ -56,7 +56,8 @@ function theme_trema_get_main_scss_content($theme) {
  * @return bool
  */
 function theme_trema_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options = array()) {
-    if ($context->contextlevel == CONTEXT_SYSTEM && ($filearea === 'logo' || $filearea === 'backgroundimage' || $filearea === 'favicon' || $filearea === 'frontpagebanner')) {
+//    if ($context->contextlevel == CONTEXT_SYSTEM && ($filearea === 'logo' || $filearea === 'backgroundimage' || $filearea === 'favicon' || $filearea === 'frontpagebanner')) {
+    if ($context->contextlevel == CONTEXT_SYSTEM ) {
         $theme = theme_config::load('trema');
         // By default, theme files must be cache-able by both browsers and proxies.
         if (!array_key_exists('cacheability', $options)) {
