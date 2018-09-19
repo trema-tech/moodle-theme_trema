5f3e3148ee8f82d1dd1c95e8e92022d812648797
Rodrigo Mady <rodrigo.mady@gmail.com>
2018-09-19 20:55:54 -0300

    [DELETE] Delete preset config

--- settings/general_settings.php
+++ settings/general_settings.php
@@ -25,37 +25,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $page = new admin_settingpage('theme_trema_general', get_string('generalsettings', 'theme_trema'));
 
-// Preset.
-$name = 'theme_trema/preset';
-$title = get_string('preset', 'theme_trema');
-$description = get_string('preset_desc', 'theme_trema');
-$default = 'default.scss';
-
-$context = context_system::instance();
-$fs = get_file_storage();
-$files = $fs->get_area_files($context->id, 'theme_trema', 'preset', 0, 'itemid, filepath, filename', false);
-
-$choices = [];
-foreach ($files as $file) {
-    $choices[$file->get_filename()] = $file->get_filename();
-}
-// These are the built in presets.
-$choices['default.scss'] = 'default.scss';
-$choices['plain.scss'] = 'plain.scss';
-
-$setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
-$setting->set_updatedcallback('theme_reset_all_caches');
-$page->add($setting);
-
-// Preset files setting.
-$name = 'theme_trema/presetfiles';
-$title = get_string('presetfiles','theme_trema');
-$description = get_string('presetfiles_desc', 'theme_trema');
-
-$setting = new admin_setting_configstoredfile($name, $title, $description, 'preset', 0,
-    array('maxfiles' => 20, 'accepted_types' => array('.scss')));
-$page->add($setting);
-
 // Favicon image setting.
 $name = 'theme_trema/favicon';
 $title = get_string('favicon', 'theme_trema');
