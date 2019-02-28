27d272e812b123b7e508e971de4b1c960957eaf1
Trevor Furtado <trevor.furtado@gmail.com>
2019-02-28 14:13:19 -0300

    Theme Trema release 3.6

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019022600;
-$plugin->release  = 'v1.1';
+$plugin->version   = 2019022800;
+$plugin->release  = '3.6.0.0';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->requires  = 2018042500;
+$plugin->requires  = 2018120300; // 3.6 (Build: 20181203)
 $plugin->component = 'theme_trema';
 $plugin->dependencies = array(
-    'theme_boost'  => 2017111300,
+    'theme_boost'  => 2018051400,
 );
