a5d19469b9c16c9125f926e8ddb8d7a584f91f0d
Rodrigo Mady <rodrigo.mady@gmail.com>
2019-08-14 17:21:38 -0300

    Theme Trema release 3.7.0.1

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019051700;
-$plugin->release  = '3.7.0.0';
+$plugin->version   = 2019081400;
+$plugin->release  = '3.7.0.1';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2018120300; // 3.6 - Build: 2018120300.
 $plugin->component = 'theme_trema';
