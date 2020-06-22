660d375966fb7f08dd4dee1e021822b5252a7b12
Rodrigo Mady <rodrigo.mady@gmail.com>
2020-06-22 11:26:53 -0300

    [ADD] Add new plugin release 3.8.0.3

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020061800;
-$plugin->release  = '3.8.0.2';
+$plugin->version   = 2020062100;
+$plugin->release  = '3.8.0.3';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2018120300; // 3.6 - Build: 2018120300.
 $plugin->component = 'theme_trema';
