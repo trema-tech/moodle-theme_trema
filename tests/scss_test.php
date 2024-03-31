784f8fc43bc0f9dbb42428303aa2d8415a4accc3
Michael Milette <michael.milette@tngconsulting.ca>
2024-03-31 03:35:15 -0400

    Added some initial PHPUnit and Behat tests.

---
+++ tests/scss_test.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+namespace theme_trema;
+
+/**
+ * Unit tests for scss compilation.
+ *
+ * @package   theme_trema
+ * @copyright 2024 TNG Consulting Inc. - {@link https://www.tngconsulting.ca/}
+ * @author    Michael Milette
+ * @copyright 2016 onwards Ankit Agarwal
+ * @license   https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class scss_test extends \advanced_testcase {
+    /**
+     * Test that trema can be compiled using SassC (the defacto implemention).
+     */
+    public function test_scss_compilation_with_sassc() {
+        if (!defined('PHPUNIT_PATH_TO_SASSC')) {
+            $this->markTestSkipped('Path to SassC not provided');
+        }
+
+        $this->resetAfterTest();
+        set_config('pathtosassc', PHPUNIT_PATH_TO_SASSC);
+
+        $this->assertNotEmpty(
+            \theme_config::load('trema')->get_css_content_debug('scss', null, null)
+        );
+    }
+}
