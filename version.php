447f808604cefeca460fe93d8872831ab8924271
Rodrigo Mady <rodrigo.mady@gmail.com>
2025-08-28 12:36:38 -0300

    Update version to 2025082800

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025082700;
+$plugin->version   = 2025082800;
 $plugin->release   = '5.0.0.3';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2022021800; // Moodle 4.0 - Build: 20220218.
