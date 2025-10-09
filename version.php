59a13988a9f977713f0604f432c6018380a86a9a
Michael Milette <michael.milette@tngconsulting.ca>
2025-10-09 11:21:08 -0400

    Updated version date.

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025091000;
-$plugin->release   = '5.0.0.3';
+$plugin->version   = 2025100900;
+$plugin->release   = '5.1.0.0';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2022021800; // Moodle 4.0 - Build: 20220218.
 $plugin->component = 'theme_trema';
