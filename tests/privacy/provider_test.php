b3b5a33c8618843d297b2455af1d362ca8e2f152
Michael Milette <michael.milette@tngconsulting.ca>
2026-04-23 14:50:54 -0400

    Updated privacy provider test for PHPUnit 11 compatibility.

--- tests/privacy/provider_test.php
+++ tests/privacy/provider_test.php
@@ -54,6 +54,7 @@ final class provider_test extends \core_privacy\tests\provider_testcase {
      *
      * @dataProvider export_user_preference_provider
      */
+    #[\PHPUnit\Framework\Attributes\DataProvider('export_user_preference_provider')]
     public function test_export_user_preferences(string $preference, bool $value, string $expectdescription): void {
         $this->resetAfterTest();
 
